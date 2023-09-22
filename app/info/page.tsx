8fbd0f149d8859f77217262076b903a6445e16ea
R0binj0 <robin.pruunlep@ametikool.ee>
2023-09-22 09:40:48 +0300

    commit

--- app/info/page.tsx
+++ app/info/page.tsx
@@ -1,7 +1,18 @@
+import Posts from "../blog/components/Posts";
+
 const Info = () => {
     return ( 
         <div>
-            Info
+            <div className='pt-6 mx-auto'>
+                <div className="flex flex-col align-center justify-center">
+                    <img className="fixed-size-image rounded-2xl" src="infoImage.png" alt="info image" />
+                </div>
+            </div>
+            <h2 className="text-6xl text-black mt-[10px] mb-[10x]">Beebide ja väikelaste esmaabi</h2>
+            <p className="text-2xl text-black mt-[10px] mb-[10x]">Õppige oskusi beebi või lapse abistamiseks hädaolukorras</p>
+            <div>
+                <Posts></Posts>
+            </div>
         </div>
      );
 }
