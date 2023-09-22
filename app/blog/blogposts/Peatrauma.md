e86af18c9c6bc5647926f0727ab927132ce7534f
R0binj0 <robin.pruunlep@ametikool.ee>
2023-09-22 10:58:06 +0300

    commit

--- app/blog/blogposts/Peatrauma.md
+++ app/blog/blogposts/Peatrauma.md
@@ -47,4 +47,4 @@ Ka treeningutesse ja kehalise kasvatuse tundidesse naasmisel soovitatakse astmel
 
 Kui sümptomid uuesti tekivad, tuleb koormust vähendada. Alati treeneriga nõu pidada ja läbi rääkida lähtuda reeglist: “mitte liiga palju ja mitte liiga vähe!“
 
-![peavigastus](../../../public/img/269291257-2eb8cd65-88b4-4ea5-8465-0e36a6ddfcfe.png)
+![peavigastus](/img/269291257-2eb8cd65-88b4-4ea5-8465-0e36a6ddfcfe.png)
--- app/info/components/Posts.tsx
+++ app/info/components/Posts.tsx
@@ -4,12 +4,23 @@ import ListItem from './ListItem'
 
 export default function Posts() {
   const posts = getSortedPostsData()
+  const firstRowPosts = posts.slice(0, 3);
+  const secondRowPosts = posts.slice(3, 5);
+  const thirdRowPosts = posts.slice(5);
   return (
     <section className='flex items-center justify-center my-14 mx-auto flex-col text-black'>
       <ul className='grid grid-cols-3 grid-rows-3 gap-8 w-3/4 pb-4'>
-          {posts.map(post => (
-              <ListItem key={post.id} post={post}></ListItem>
-          ))}
+        {firstRowPosts.map((post) => (
+          <ListItem key={post.id} post={post}></ListItem>
+        ))}
+
+        {secondRowPosts.map((post) => (
+          <ListItem key={post.id} post={post}></ListItem>
+        ))}
+
+        {thirdRowPosts.map((post) => (
+          <ListItem key={post.id} post={post}></ListItem>
+        ))}
       </ul>
     </section>
   )
